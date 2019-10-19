9225b0dac85107df142f0383e9b0d4f261d7cd51
Anton Österberg <anton.osterberg@gmail.com>
2019-10-19 15:45:47 +0200

    Remove default audience

--- src/main/kotlin/io/klira/auth0/m2m/ServiceConfig.kt
+++ src/main/kotlin/io/klira/auth0/m2m/ServiceConfig.kt
@@ -5,9 +5,9 @@ data class ServiceConfig(
     private val config: Map<String, String> = System.getenv()
 ): Map<String, String> by config {
 
-    val audience: String = config[AUDIENCE] ?: DEFAULT_AUDIENCE
     val clientId: String = config[ID] ?: error("Missing $ID")
     val clientSecret: String = config[SECRET] ?: error("Missing $SECRET")
+    fun audience(): String = config[AUDIENCE] ?: error("Missing $AUDIENCE")
 
     fun withClientId(clientId: String): ServiceConfig =
         this.copy(config = config.plus(ID to clientId))
@@ -22,7 +22,5 @@ data class ServiceConfig(
         private const val AUDIENCE = "AUTH0_AUDIENCE"
         private const val ID = "AUTH0_CLIENT_ID"
         private const val SECRET = "AUTH0_CLIENT_SECRET"
-
-        const val DEFAULT_AUDIENCE = "https://RETRACTED"
     }
 }
